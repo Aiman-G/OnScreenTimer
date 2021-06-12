734246ad7ae7cb7b1241f2925717747128869a14
aymen111 <aymen.omg@gmail.com>
2021-06-12 14:42:18 +0800

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,137 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.lblMin = New System.Windows.Forms.Label()
+        Me.lblSec = New System.Windows.Forms.Label()
+        Me.TopmostTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(54, 120)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(109, 42)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Strat"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'lblMin
+        '
+        Me.lblMin.AllowDrop = True
+        Me.lblMin.AutoSize = True
+        Me.lblMin.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lblMin.ForeColor = System.Drawing.Color.RoyalBlue
+        Me.lblMin.Location = New System.Drawing.Point(112, 9)
+        Me.lblMin.Name = "lblMin"
+        Me.lblMin.Size = New System.Drawing.Size(36, 28)
+        Me.lblMin.TabIndex = 1
+        Me.lblMin.Text = "00"
+        '
+        'lblSec
+        '
+        Me.lblSec.AllowDrop = True
+        Me.lblSec.AutoSize = True
+        Me.lblSec.Font = New System.Drawing.Font("Segoe UI", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lblSec.ForeColor = System.Drawing.Color.RoyalBlue
+        Me.lblSec.Location = New System.Drawing.Point(75, 12)
+        Me.lblSec.Name = "lblSec"
+        Me.lblSec.Size = New System.Drawing.Size(41, 25)
+        Me.lblSec.TabIndex = 1
+        Me.lblSec.Text = "00: "
+        '
+        'TopmostTimer
+        '
+        Me.TopmostTimer.Interval = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(53, -142)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 20)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(169, 158)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(173, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Aiman- CLoud Lab 210 "
+        '
+        'btnStart
+        '
+        Me.btnStart.Location = New System.Drawing.Point(2, 6)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(76, 41)
+        Me.btnStart.TabIndex = 4
+        Me.btnStart.Text = "Start"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AllowDrop = True
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(186, 70)
+        Me.Controls.Add(Me.btnStart)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lblSec)
+        Me.Controls.Add(Me.lblMin)
+        Me.Controls.Add(Me.Button1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow
+        Me.Name = "Form1"
+        Me.Opacity = 0.8R
+        Me.Text = "Aiman- CLoud Lab 210 "
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Button1 As Button
+    Friend WithEvents lblMin As Label
+    Friend WithEvents lblSec As Label
+    Friend WithEvents TopmostTimer As Timer
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnStart As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,66 @@
+﻿' Wrttien by Ghannami ; Aymen.omg@gmail.com
+
+' A simple timer for Microsoft PowerPoint  presentation
+'
+
+
+Public Class Form1
+
+    Dim secondes As Integer
+    Dim minutes As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Me.TopMost = True
+        lblSec.BackColor = System.Drawing.Color.Transparent
+        lblMin.BackColor = System.Drawing.Color.Transparent
+
+        'set controlbox flase
+        Me.ControlBox = False
+        ' set timer interval to 1 second /1000ms
+        Timer1.Interval = 1000
+
+
+        ' Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Show()
+        TopmostTimer.Enabled = True
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Timer1.Enabled = True
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        secondes += 1
+        If secondes = 60 Then
+            minutes += 1
+            secondes = 0
+        End If
+        lblSec.Text = CStr(secondes) + ":"
+        lblMin.Text = CStr(minutes)
+    End Sub
+
+    Private Sub TopmostTimer_Tick(sender As Object, e As EventArgs) Handles TopmostTimer.Tick
+
+        Me.TopMost = True
+
+    End Sub
+
+
+
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        Timer1.Enabled = True
+    End Sub
+
+
+
+
+
+
+    'Private Sub Form1_Paint(sender As Object, e As PaintEventArgs) Handles MyBase.Paint
+    '    Dim grapics As Graphics = Me.CreateGraphics()
+
+    '    grapics.FillEllipse(System.Drawing.Brushes.Blue, 0, 0, Me.ClientSize.Width - 5,
+    '        Me.ClientSize.Height - 5)
+    'End Sub
+End Class
